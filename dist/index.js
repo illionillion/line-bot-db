dfbd613556905f07e6ca2cc078c1c47ea06e7a12
yuto ichinose <yuto.0526.sano@gmail.com>
2024-01-14 01:03:47 +0900

    feat: 削除機能実装

--- dist/index.js
+++ dist/index.js
@@ -41,6 +41,8 @@ const app = (0, express_1.default)();
 // view engine setup
 app.set("views", path_1.default.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
 app.use("/public", express_1.default.static(path_1.default.join(__dirname, "/public/")));
 app.get("/", (_, res) => __awaiter(void 0, void 0, void 0, function* () {
     db.serialize(() => {
@@ -54,6 +56,12 @@ app.get("/", (_, res) => __awaiter(void 0, void 0, void 0, function* () {
         });
     });
 }));
+app.post("/delete", function (req, res, next) {
+    const { id } = req.body;
+    //SQL文, DataBaseのレコード作成
+    db.run("delete from message_table where id = ?", id);
+    res.redirect("/");
+});
 const textEventHandler = (event) => __awaiter(void 0, void 0, void 0, function* () {
     if (event.type !== "message") {
         return;
--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,8 @@ const app: Application = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use("/public", express.static(path.join(__dirname, "/public/")));
 
 app.get("/", async (_: Request, res: Response) => {
@@ -54,6 +55,16 @@ app.get("/", async (_: Request, res: Response) => {
       }
     });
   });
+});
+
+app.post("/delete", function (req, res, next) {
+  const { id } = req.body;
+  //SQL文, DataBaseのレコード作成
+  db.run(
+    "delete from message_table where id = ?",
+    id
+  );
+  res.redirect("/")
 });
 
 const textEventHandler = async (
